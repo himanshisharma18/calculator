1df9499ae4e6dd5a4feb6905ed6cee1c375c06d8
himanshisharma18 <godhimanshisharma@gmail.com>
2025-07-21 11:15:20 +0530

    Add files via upload

---
+++ calculator/script2.js
@@ -0,0 +1,19 @@
+
+let string = "";
+let buttons = document.querySelectorAll('.button');
+buttons.forEach(function(button) {
+  button.addEventListener("click", function(e) {
+    let value = e.target.innerHTML;
+
+    if (value === "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else if (value === "C") {
+      string = "";
+      document.querySelector("input").value = string;
+    } else {
+      string += value;
+      document.querySelector("input").value = string;
+    }
+  }); 
+});
